8cfc266d61719c018d0c08435834f09a4ff37020
dddddeeeevvvaaor <prizandevaokturarizqy@gmail.com>
2022-09-16 04:05:15 +0700

    first commit

---
+++ src/main/java/com/example/data/penduduk/Data/Penduduk/DataPendudukApplication.java
@@ -0,0 +1,13 @@
+package com.example.data.penduduk.Data.Penduduk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataPendudukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataPendudukApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/data/penduduk/Data/Penduduk/DataPendudukApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.data.penduduk.Data.Penduduk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataPendudukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
