45c37a3d571fc2d7771186bcdbe534569209812e
dddddeeeevvvaaor <prizandevaokturarizqy@gmail.com>
2022-09-16 14:13:31 +0700

    entitis kematian

---
+++ src/main/java/com/example/data/penduduk/Data/Penduduk/models/entities/kematian.java
@@ -0,0 +1,10 @@
+package com.example.data.penduduk.Data.Penduduk.models.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tbl_datakematian")
+public class kematian {
+    
+}
