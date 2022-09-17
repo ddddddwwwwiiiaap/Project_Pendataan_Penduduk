5d60e41255ddc760099dcbe8bb69a254e6f0f092
dwiaprilyaanggoroputry <dwi.aprilya.ft20@mail.umy.ac.id>
2022-09-17 10:56:27 +0700

    UP

--- src/main/java/com/example/data/penduduk/Data/Penduduk/models/entities/PendataanPenduduk.java
+++ src/main/java/com/example/data/penduduk/Data/Penduduk/models/entities/PendataanPenduduk.java
@@ -10,7 +10,7 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="tbl_penduduk")
+@Table(name="tbl_pendataanPenduduk")
 public class PendataanPenduduk implements Serializable{
 
     private static final long serialVersionUID = 1L;
