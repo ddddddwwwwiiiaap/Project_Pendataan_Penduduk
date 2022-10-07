fc4c2c59e5c283d0cab86747c7b3b5eabe5108f4
ddddddwwwwiiiaap <dwiaprilyaanggoroputry02@gmail.com>
2022-10-07 22:09:50 +0700

    .

--- src/main/java/com/example/data/penduduk/Data/Penduduk/models/repos/UserRepo.java
+++ src/main/java/com/example/data/penduduk/Data/Penduduk/models/repos/UserRepo.java
@@ -7,5 +7,6 @@ import org.springframework.data.repository.CrudRepository;
 import com.example.data.penduduk.Data.Penduduk.models.entities.User;
 
 public interface UserRepo extends CrudRepository<User, Long>{
+    
     List<User> findByUsernameContains(String username);
 }
