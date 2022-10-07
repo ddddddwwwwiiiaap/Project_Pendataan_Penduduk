1a112adcf996b8270368cc31c296dbc4a6c79bc0
ddddddwwwwiiiaap <dwiaprilyaanggoroputry02@gmail.com>
2022-10-07 22:01:28 +0700

    .

--- src/main/java/com/example/data/penduduk/Data/Penduduk/models/repos/PendataanPendudukRepo.java
+++ src/main/java/com/example/data/penduduk/Data/Penduduk/models/repos/PendataanPendudukRepo.java
@@ -7,10 +7,5 @@ import org.springframework.data.repository.CrudRepository;
 import com.example.data.penduduk.Data.Penduduk.models.entities.PendataanPenduduk;
 
 public interface PendataanPendudukRepo extends CrudRepository<PendataanPenduduk, Long>{
-<<<<<<< HEAD
     List<PendataanPenduduk> findByNamaContains(String nama);
-=======
-    
-  //  List<PendataanPenduduk> findByNameConstains(String nama);
->>>>>>> fd0ee351c3f56dd87a35397437b2cfb7b91c2264
 }
