f837608f6aac8855705c3683662ee0daae392375
dddddeeeevvvaaor <prizandevaokturarizqy@gmail.com>
2022-10-24 21:54:27 +0700

    membuat dto kematiandata

---
+++ src/main/java/com/example/data/penduduk/Data/Penduduk/dto/KematianData.java
@@ -0,0 +1,89 @@
+package com.example.data.penduduk.Data.Penduduk.dto;
+
+import javax.persistence.Column;
+
+public class KematianData {
+    
+    @Column(name="nik",columnDefinition="char(16)", length = 16)
+    private String nik;
+
+    private String nama;
+
+    private String alamat;
+
+    private String jenisKelamin;
+
+    private String agama;
+
+    private String tempatLahir;
+
+    private String tanggalLahir;
+
+    private String kewarganegaraan;
+
+    public String getNik() {
+        return nik;
+    }
+
+    public void setNik(String nik) {
+        this.nik = nik;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    public void setJenisKelamin(String jenisKelamin) {
+        this.jenisKelamin = jenisKelamin;
+    }
+
+    public String getAgama() {
+        return agama;
+    }
+
+    public void setAgama(String agama) {
+        this.agama = agama;
+    }
+
+    public String getTempatLahir() {
+        return tempatLahir;
+    }
+
+    public void setTempatLahir(String tempatLahir) {
+        this.tempatLahir = tempatLahir;
+    }
+
+    public String getTanggalLahir() {
+        return tanggalLahir;
+    }
+
+    public void setTanggalLahir(String tanggalLahir) {
+        this.tanggalLahir = tanggalLahir;
+    }
+
+    public String getKewarganegaraan() {
+        return kewarganegaraan;
+    }
+
+    public void setKewarganegaraan(String kewarganegaraan) {
+        this.kewarganegaraan = kewarganegaraan;
+    }
+
+    
+}
