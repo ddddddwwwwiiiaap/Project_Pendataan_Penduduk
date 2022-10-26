b4fa8df7220e047ebc2c81516fd10f60cf05f6bf
dddddeeeevvvaaor <prizandevaokturarizqy@gmail.com>
2022-10-26 16:06:48 +0700

    menambahkan PostMaping PendataanPenduduk

--- src/main/java/com/example/data/penduduk/Data/Penduduk/services/KematianPendudukService.java
+++ src/main/java/com/example/data/penduduk/Data/Penduduk/services/KematianPendudukService.java
@@ -8,8 +8,11 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.data.penduduk.Data.Penduduk.dto.KematianData;
 import com.example.data.penduduk.Data.Penduduk.models.entities.KematianPenduduk;
+import com.example.data.penduduk.Data.Penduduk.models.entities.PendataanPenduduk;
 import com.example.data.penduduk.Data.Penduduk.models.repos.KematianPendudukRepo;
+import com.example.data.penduduk.Data.Penduduk.models.repos.PendataanPendudukRepo;
 
 @Service
 @Transactional
@@ -18,13 +21,17 @@ public class KematianPendudukService {
     @Autowired
     private KematianPendudukRepo kematianPendudukRepo;
 
+    @Autowired
+    private PendataanPendudukRepo pendataanPendudukRepo;
+    
+
     public KematianPenduduk save(KematianPenduduk kematianPenduduk) {
         return kematianPendudukRepo.save(kematianPenduduk);
     }
 
-    public KematianPenduduk findOne(Long id){
+    public KematianPenduduk findOne(Long id) {
         Optional<KematianPenduduk> kematianPenduduk = kematianPendudukRepo.findById(id);
-        if(!kematianPenduduk.isPresent()){
+        if (!kematianPenduduk.isPresent()) {
             return null;
         }
         return kematianPenduduk.get();
@@ -38,7 +45,26 @@ public class KematianPendudukService {
         kematianPendudukRepo.deleteById(id);
     }
 
+    // public PendataanPenduduk get(PendataanPenduduk pendataanPenduduk) {
+    // return pendataanPenduduk;
+    // }
+
+    // public PendataanPenduduk findOne(String nik){
+    /*
+     * Optional<PendataanPenduduk> pendataanPenduduk =
+     * kematianPendudukRepo.findByNik(nik);
+     * if(!pendataanPenduduk.isPresent()){
+     * return null;
+     * }
+     */
+    // return KematianPendudukRepo.findByNik(nik);
+    // }
+
     public List<KematianPenduduk> findByName(String tempatkematian) {
         return kematianPendudukRepo.findBytempatkematianContaining(tempatkematian);
+    }
+
+    public PendataanPenduduk findByNik(String nik) {
+        return pendataanPendudukRepo.findByNikContains(nik);
     }
 }
