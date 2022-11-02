d62ce695a0117bfeb8da49a24d2ec2981e7d8cc7
ddddddwwwwiiiaap <dwiaprilyaanggoroputry02@gmail.com>
2022-11-02 19:54:45 +0700

    Menambahkan annotations Swaggger2

--- src/main/java/com/example/data/penduduk/Data/Penduduk/DataPendudukApplication.java
+++ src/main/java/com/example/data/penduduk/Data/Penduduk/DataPendudukApplication.java
@@ -5,7 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class DataPendudukApplication {
 
 	public static void main(String[] args) {
@@ -16,4 +22,13 @@ public class DataPendudukApplication {
 	public ModelMapper modelMapper() {
 	    return new ModelMapper();
 	}
+
+	@Bean
+	public Docket productApi() {
+	   return new Docket(DocumentationType.SWAGGER_2).select()
+		  .apis(RequestHandlerSelectors.basePackage("com.example.data.penduduk.Data.Penduduk")).build();
+		  
+	}
+
+	
 }
