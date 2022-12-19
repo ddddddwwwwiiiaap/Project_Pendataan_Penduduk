0710e6159cdcbf75cc0a8f9ece97b21db387df98
ddddddwwwwiiiaap <dwiaprilyaanggoroputry02@gmail.com>
2022-12-19 23:48:49 +0700

    Add Search  pada HomeController

--- src/main/java/com/example/data/penduduk/Data/Penduduk/controllers/HomeController.java
+++ src/main/java/com/example/data/penduduk/Data/Penduduk/controllers/HomeController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.data.penduduk.Data.Penduduk.dto.SearchData;
 import com.example.data.penduduk.Data.Penduduk.services.KematianPendudukService;
 import com.example.data.penduduk.Data.Penduduk.services.PendataanPendudukService;
 import com.example.data.penduduk.Data.Penduduk.services.PindahPendudukService;
@@ -25,9 +27,17 @@ public class HomeController {
 
     @GetMapping
     public String index(Model model) {
+        model.addAttribute("searchData", new SearchData());
         model.addAttribute("pendataan", pendataanPendudukService.findAll());
         model.addAttribute("pindah", pindahPendudukService.findAll());
         model.addAttribute("kematian", kematianPendudukService.findAll());
+        return "index";
+    }
+
+    @PostMapping("/search")
+    public String search(SearchData searchData, Model model){
+        model.addAttribute("searchData", searchData);
+        model.addAttribute("pendataan", pendataanPendudukService.findByName(searchData.getSearchKey()));
         return "index";
     }
 }
