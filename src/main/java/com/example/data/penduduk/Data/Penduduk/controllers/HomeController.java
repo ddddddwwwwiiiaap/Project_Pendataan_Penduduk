1cb4360853e2a30eefb13672a98407ef9a017200
dddddeeeevvvaaor <prizandevaokturarizqy@gmail.com>
2022-12-19 16:38:23 +0700

    memperbaiki sintak

---
+++ src/main/java/com/example/data/penduduk/Data/Penduduk/controllers/HomeController.java
@@ -0,0 +1,29 @@
+package com.example.data.penduduk.Data.Penduduk.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.data.penduduk.Data.Penduduk.services.KematianPendudukService;
+import com.example.data.penduduk.Data.Penduduk.services.PendataanPendudukService;
+import com.example.data.penduduk.Data.Penduduk.services.PindahPendudukService;
+
+@Controller
+@RequestMapping("")
+public class HomeController {
+
+    @Autowired
+    private PindahPendudukService pindahPendudukService;
+
+    @Autowired
+    private KematianPendudukService kematianPendudukService;
+
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("pindah", pindahPendudukService.findAll());
+        model.addAttribute("kematian", kematianPendudukService.findAll());
+        return "index";
+    }
+}
