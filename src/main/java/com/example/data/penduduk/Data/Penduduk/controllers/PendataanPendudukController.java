2ab3a3ae951f6aa396f412e71f959cb78d6e2073
ddddddwwwwiiiaap <dwiaprilyaanggoroputry02@gmail.com>
2022-12-21 14:51:22 +0700

    Fix syntax pendataan penduduk

--- src/main/java/com/example/data/penduduk/Data/Penduduk/controllers/PendataanPendudukController.java
+++ src/main/java/com/example/data/penduduk/Data/Penduduk/controllers/PendataanPendudukController.java
@@ -1,18 +1,9 @@
 package com.example.data.penduduk.Data.Penduduk.controllers;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,9 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.example.data.penduduk.Data.Penduduk.dto.ResponseData;
 import com.example.data.penduduk.Data.Penduduk.dto.SearchData;
 import com.example.data.penduduk.Data.Penduduk.models.entities.PendataanPenduduk;
 import com.example.data.penduduk.Data.Penduduk.services.PendataanPendudukService;
