74bd8271aa51a39bfc7a70a986705e7672a7196b
dddddeeeevvvaaor <prizandevaokturarizqy@gmail.com>
2022-12-25 23:18:38 +0700

    add optional di kematian penduduk repo

--- src/main/java/com/example/data/penduduk/Data/Penduduk/models/repos/KematianPendudukRepo.java
+++ src/main/java/com/example/data/penduduk/Data/Penduduk/models/repos/KematianPendudukRepo.java
@@ -1,6 +1,7 @@
 package com.example.data.penduduk.Data.Penduduk.models.repos;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 
@@ -9,4 +10,6 @@ import com.example.data.penduduk.Data.Penduduk.models.entities.KematianPenduduk;
 public interface KematianPendudukRepo extends CrudRepository<KematianPenduduk, Long> {
 
     List<KematianPenduduk> findBytempatkematianContaining(String tempatkematian);
+
+    Optional<KematianPenduduk> findById(Long id);
 }
