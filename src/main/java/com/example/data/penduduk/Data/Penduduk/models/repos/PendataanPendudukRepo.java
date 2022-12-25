b673eb73191daaf839a0206a86933979b6a01c37
ddddddwwwwiiiaap <dwiaprilyaanggoroputry02@gmail.com>
2022-12-25 16:51:14 +0700

    Add findById pada PendataanPendudukRepo

--- src/main/java/com/example/data/penduduk/Data/Penduduk/models/repos/PendataanPendudukRepo.java
+++ src/main/java/com/example/data/penduduk/Data/Penduduk/models/repos/PendataanPendudukRepo.java
@@ -1,17 +1,20 @@
 package com.example.data.penduduk.Data.Penduduk.models.repos;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.data.penduduk.Data.Penduduk.models.entities.PendataanPenduduk;
 
-public interface PendataanPendudukRepo extends CrudRepository<PendataanPenduduk, String>{
+public interface PendataanPendudukRepo extends CrudRepository<PendataanPenduduk, Long>{
 
     List<PendataanPenduduk> findByNamaContains(String nama); //nama method harus sesuai dengan nama entity
 
     PendataanPenduduk findByNikContains(String nik);
 
     PendataanPenduduk findByNik(String nik);
+
+    Optional<PendataanPenduduk> findById(Long id);
 
 }
